466248a7cd8bc6c3934e268f55b09ee107a126a0
Somesh Ranjan <somesh.ranjan.met20@itbhu.ac.in>
2022-06-10 12:13:09 +0530

    chore: Add custom links for navigation
    Signed-off-by: Somesh Ranjan <somesh.ranjan.met20@itbhu.ac.in>

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,25 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
+import { createRouter, createWebHashHistory } from 'vue-router';
+import Home from '@/views/Home.vue';
+import About from '@/views/About.vue';
+import Manage from '@/views/Manage.vue';
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
     component: Home,
   },
   {
     path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: About,
+  },
+  {
+    path: '/manage',
+    component: Manage,
   },
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes,
 });
 
