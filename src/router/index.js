80f3494e5ef4d0bc2aea522e8a0a4270230cda68
Somesh Ranjan <someshranjan252008@gmail.com>
2022-06-19 17:46:41 +0530

    Add checks for redirection in router

--- src/router/index.js
+++ src/router/index.js
@@ -2,19 +2,38 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from '@/views/Home.vue';
 import About from '@/views/About.vue';
 import Manage from '@/views/Manage.vue';
+import store from '@/store';
 
 const routes = [
   {
+    name: 'home',
     path: '/',
     component: Home,
   },
   {
+    name: 'about',
     path: '/about',
     component: About,
   },
   {
-    path: '/manage',
+    name: 'manage',
+    path: '/manage-music',
+    meta: {
+      requiresAuth: true,
+    },
     component: Manage,
+    beforeEnter: (to, from, next) => {
+      console.log('Manage route guard');
+      next();
+    },
+  },
+  {
+    path: '/manage',
+    redirect: { name: 'manage' },
+  },
+  {
+    path: '/:catchAll(.*)*',
+    redirect: { name: 'home' },
   },
 ];
 
@@ -22,6 +41,20 @@ const router = createRouter({
   history: createWebHashHistory(process.env.BASE_URL),
   routes,
   linkExactActiveClass: 'text-orange-500',
+});
+
+router.beforeEach((to, from, next) => {
+  // console.log(to.matched);
+
+  if (!to.matched.some((record) => record.meta.requiresAuth)) {
+    next();
+    return;
+  }
+  if (store.state.userLoggedIn) {
+    next();
+  } else {
+    next({ name: 'home' });
+  }
 });
 
 export default router;
