428a8ad4a084e5753a17a5da81b709c950dd5611
Somesh Ranjan <somesh.ranjan.met20@itbhu.ac.in>
2022-06-22 19:06:46 +0530

    feat: Add validation to the song form

--- src/components/CompositionItem.vue
+++ src/components/CompositionItem.vue
@@ -1,29 +1,33 @@
 <template>
   <div class="border border-gray-200 p-3 mb-4 rounded">
-              <div>
-                <h4 class="inline-block text-2xl font-bold">Song Name</h4>
+              <div v-show="!displayForm">
+                <h4 class="inline-block text-2xl font-bold">{{ song.modified_name }}</h4>
                 <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right">
                   <i class="fa fa-times"></i>
                 </button>
-                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right">
+                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right"
+                @click.prevent="displayForm = !displayForm">
                   <i class="fa fa-pencil-alt"></i>
                 </button>
               </div>
-              <div>
-                <form>
+              <div v-show="displayForm">
+                <vee-form :validation-schema="validationSchema" :initial-values="song"
+                @submit="editSong">
                   <div class="mb-3">
                     <label class="inline-block mb-2">Song Title</label>
-                    <input type="text"
+                    <vee-field type="text" name="modified_name"
                       class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300
                         transition duration-500 focus:outline-none focus:border-black rounded"
                       placeholder="Enter Song Title" />
+                      <ErrorMessage class="text-red-600" name="modified_name" />
                   </div>
                   <div class="mb-3">
                     <label class="inline-block mb-2">Genre</label>
-                    <input type="text"
+                    <vee-field type="text" name="genre"
                       class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300
                         transition duration-500 focus:outline-none focus:border-black rounded"
                       placeholder="Enter Genre" />
+                      <ErrorMessage class="text-red-600" name="genre" />
                   </div>
                   <button type="submit" class="py-1.5 px-3 rounded text-white bg-green-600">
                     Submit
@@ -31,7 +35,7 @@
                   <button type="submit" class="py-1.5 px-3 rounded text-white bg-gray-600">
                     Go Back
                   </button>
-                </form>
+                </vee-form>
               </div>
             </div>
 </template>
@@ -43,6 +47,20 @@ export default {
     song: {
       type: Object,
       reuired: true,
+    },
+  },
+  data() {
+    return {
+      displayForm: false,
+      validationSchema: {
+        modified_name: 'required',
+        genre: 'alpha_spaces',
+      },
+    };
+  },
+  methods: {
+    editSong() {
+      console.log('Song edited');
     },
   },
 };
