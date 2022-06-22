c7dab124afbf6754e391e0be4c6fdacb604453fe
Somesh Ranjan <someshranjan252008@gmail.com>
2022-06-22 16:13:53 +0530

    chore: Cancel upload before unmounting the component

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -105,5 +105,10 @@ export default {
       console.log(files);
     },
   },
+  beforeUnmount() {
+    this.uploads.forEach((upload) => {
+      upload.uploadTask.cancel();
+    });
+  },
 };
 </script>
