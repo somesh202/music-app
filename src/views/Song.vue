9930a4ecb7cefda600b5aa0a6f10a41bcf8c84c9
Somesh Ranjan <somesh.ranjan.met20@itbhu.ac.in>
2022-06-24 19:05:36 +0530

    Add query params for two sorts

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -105,6 +105,9 @@ export default {
       return;
     }
 
+    const { sort } = this.$route.query;
+
+    this.sort = sort === '1' || sort === '2' ? sort : '1';
     this.song = docSnapshot.data();
     this.getComments();
   },
@@ -146,6 +149,18 @@ export default {
           ...doc.data(),
         }),
       ]);
+    },
+  },
+  watch: {
+    sort(newVal) {
+      if (newVal === this.$route.query.sort) {
+        return;
+      }
+      this.$router.push({
+        query: {
+          sort: newVal,
+        },
+      });
     },
   },
 };
