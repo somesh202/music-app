47fa30ba9822312a7594ddd1bc1370a47ca90ced
Somesh Ranjan <somesh.ranjan.met20@itbhu.ac.in>
2022-06-25 16:32:08 +0530

    feat: Add mutations for playing audio

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,13 @@
 import { createStore } from 'vuex';
 import { auth, usersCollection } from '@/includes/firebase';
+import { Howl } from 'howler';
 
 export default createStore({
   state: {
     authModalShow: false,
     userLoggedIn: false,
+    currentSong: {},
+    audio: {},
   },
   mutations: {
     toggleAuthModal: (state) => {
@@ -12,6 +15,13 @@ export default createStore({
     },
     toggleAuth: (state) => {
       state.userLoggedIn = !state.userLoggedIn;
+    },
+    newSong(state, payload) {
+      state.currentSong = payload;
+      state.audio = new Howl({
+        src: [payload.url],
+        html5: true,
+      });
     },
   },
   getters: {
@@ -49,6 +59,11 @@ export default createStore({
       await auth.signOut();
 
       commit('toggleAuth');
+    },
+    async newSong({ commit, state }, payload) {
+      commit('newSong', payload);
+
+      state.audio.play();
     },
   },
 });
