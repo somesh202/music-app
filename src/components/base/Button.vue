e68f280504afe9d2fb4740f4ca7017cadc0f31d2
Somesh Ranjan <somesh.ranjan.met20@itbhu.ac.in>
2022-07-05 19:44:08 +0530

    feat: Add logic for auto-registering global components

---
+++ src/components/base/Button.vue
@@ -0,0 +1,5 @@
+<template>
+    <button type="button" class="btn btn-primary" :class="{'btn-outline-primary':outline}">
+        Press Me
+    </button>
+</template>
--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import Icon from './directives/icon';
 import './assets/tailwind.css';
 import './assets/main.css';
 import './registerServiceWorker';
+import GlobalComponents from './includes/_globals';
 
 let app;
 
@@ -18,6 +19,7 @@ auth.onAuthStateChanged(() => {
     app.use(store);
     app.use(router);
     app.use(VeeValidatePlugin);
+    app.use(GlobalComponents);
     app.directive('icon', Icon);
     app.mount('#app');
   }
